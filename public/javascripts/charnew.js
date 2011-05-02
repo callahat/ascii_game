aeb9188213b8f905f9d69bda97cd9ad2fd2194eb
callahat <tim.callahan25@yahoo.com>
2011-05-02 00:54:05 +0000

    jquery noconflict mode

--- public/javascripts/charnew.js
+++ public/javascripts/charnew.js
@@ -1,14 +1,16 @@
-( function($) {
+jQuery.noConflict();
+
+//( function(jQuery) {
 
 
-  $(document).ready(function(){
+  jQuery(document).ready(function(){
     window.onload = function(){ 
-        changeStuff("c_class_id", $("select[@id=c_class_id]")[0].value);
-        changeStuff("race_id", $("select[@id=race_id]")[0].value);
+        changeStuff("c_class_id", jQuery("select[@id=c_class_id]")[0].value);
+        changeStuff("race_id", jQuery("select[@id=race_id]")[0].value);
         updateCumStat();
     };
     
-    $("select").change(function(event) {
+    jQuery("select").change(function(event) {
       //alert(this.value);
       //alert(this.id);
       changeStuff(this.id, this.value);
@@ -19,23 +21,23 @@
    
      //change the displayed stats
      function changeStuff(what, id){
-       //alert($("input[@class="+what+"][@id="+id+"]").length);
-       $("input[@class="+what+"][@id="+id+"]").each(function(i){
-         //alert($("input[@class="+what+"][@id="+id+"]")[i].value);
-         $("div[@class="+what+"_stat]")[i].innerHTML = $("input[@class="+what+"][@id="+id+"]")[i].value;
+       //alert(jQuery("input[@class="+what+"][@id="+id+"]").length);
+       jQuery("input[@class="+what+"][@id="+id+"]").each(function(i){
+         //alert(jQuery("input[@class="+what+"][@id="+id+"]")[i].value);
+         jQuery("div[@class="+what+"_stat]")[i].innerHTML = jQuery("input[@class="+what+"][@id="+id+"]")[i].value;
        });
      }
      
      //update the displayed stats
      function updateCumStat() {
        //alert("Got here");
-       $("div[@class=statsum]").each(function(i){
-         //alert(parseFloat($("div[@class=c_class_id_stat]")[i+1].innerHTML));
-         $("div[@class=statsum]")[i].innerHTML = parseFloat($("div[@class=c_class_id_stat]")[i+1].innerHTML) + parseFloat($("div[@class=race_id_stat]")[i+1].innerHTML);
+       jQuery("div[@class=statsum]").each(function(i){
+         //alert(parseFloat(jQuery("div[@class=c_class_id_stat]")[i+1].innerHTML));
+         jQuery("div[@class=statsum]")[i].innerHTML = parseFloat(jQuery("div[@class=c_class_id_stat]")[i+1].innerHTML) + parseFloat(jQuery("div[@class=race_id_stat]")[i+1].innerHTML);
        });
      }
      
   }); //end document
   
 
-} ) ( jQuery );
+//} ) ( jQuery );
--- public/javascripts/raiselevel.js
+++ public/javascripts/raiselevel.js
@@ -1,38 +1,40 @@
-( function($) {
+jQuery.noConflict();
 
-  $(document).ready(function(){
+//( function(jQuery) {
+
+  jQuery(document).ready(function(){
     window.onload = function(){ 
         update_freepts();
     };
     
-    $("input[@type=button]").click(function(event) {
+    jQuery("input[@type=button]").click(function(event) {
       var sum;
       if(this.value == "-"){
-        sum = parseInt($("input[@type=text][@id="+this.id+"]").val()) - 1;
-      }else if(parseInt($("#freepoint_sum").text()) > 0){
-        sum = parseInt($("input[@type=text][@id="+this.id+"]").val()) + 1;
+        sum = parseInt(jQuery("input[@type=text][@id="+this.id+"]").val()) - 1;
+      }else if(parseInt(jQuery("#freepoint_sum").text()) > 0){
+        sum = parseInt(jQuery("input[@type=text][@id="+this.id+"]").val()) + 1;
       }
       if(sum >= 0){
-        $("input[@type=text][@id="+this.id+"]").val(sum);
+        jQuery("input[@type=text][@id="+this.id+"]").val(sum);
       }
       update_freepts();
      })
      
-     $("input[@type=text]").change(function(event) {
+     jQuery("input[@type=text]").change(function(event) {
        update_freepts();
      })
      
      
      //functions
      function update_freepts(){
-       var attrs = $("input[@type=text]");
+       var attrs = jQuery("input[@type=text]");
        var freesum = 0;
        attrs.each(function(i){
-         freesum -= parseInt($("input[@type=text]")[i].value);
+         freesum -= parseInt(jQuery("input[@type=text]")[i].value);
        });
-       freesum += parseInt($("input[@id=total_freepts]").val());
-       $("#freepoint_sum").text(freesum);
+       freesum += parseInt(jQuery("input[@id=total_freepts]").val());
+       jQuery("#freepoint_sum").text(freesum);
      }
   }); //end document
 
-} ) ( jQuery );
+//} ) ( jQuery );
