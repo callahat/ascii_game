a2b694760fbe26029ed2e9dad1e8f44bc0dccc48
callahat <tim.callahan25@yahoo.com>
2011-05-02 01:09:42 +0000

    Cap the max distributed freept value on the raise_level page

--- public/javascripts/raiselevel.js
+++ public/javascripts/raiselevel.js
@@ -18,23 +18,36 @@ jQuery.noConflict();
         jQuery("input[@type=text][@id="+this.id+"]").val(sum);
       }
       update_freepts();
-     })
-     
-     jQuery("input[@type=text]").change(function(event) {
-       update_freepts();
-     })
-     
-     
-     //functions
-     function update_freepts(){
-       var attrs = jQuery("input[@type=text]");
-       var freesum = 0;
-       attrs.each(function(i){
-         freesum -= parseInt(jQuery("input[@type=text]")[i].value);
-       });
-       freesum += parseInt(jQuery("input[@id=total_freepts]").val());
-       jQuery("#freepoint_sum").text(freesum);
-     }
+    })
+    
+    jQuery("input[@type=text]").change(function(event) {
+      var sum = parseInt(jQuery("input[@type=text][@id="+this.id+"]").val());
+      var freePts = parseInt(jQuery("#freepoint_sum").text());
+      sum = (sum > 0 ? sum : 0);
+      
+      if( freePts - sum >= 0){
+        jQuery("input[@type=text][@id="+this.id+"]").val(sum);
+      } else {
+        jQuery("input[@type=text][@id="+this.id+"]").val(freePts);
+      }
+      update_freepts();
+    })
+    
+    jQuery("input[@type=text]").change(function(event) {
+      update_freepts();
+    })
+    
+    
+    //functions
+    function update_freepts(){
+      var attrs = jQuery("input[@type=text]");
+      var freesum = 0;
+      attrs.each(function(i){
+        freesum -= parseInt(jQuery("input[@type=text]")[i].value);
+      });
+      freesum += parseInt(jQuery("input[@id=total_freepts]").val());
+      jQuery("#freepoint_sum").text(freesum);
+    }
   }); //end document
 
 //} ) ( jQuery );
