db5f87ccd2e8a43d04ceef63696f9b0894576c53
callahat <tim.callahan25@yahoo.com>
2014-07-01 02:16:29 +0000

    Fixing a bug with the stat calculation, and some other bugs with displaying some characterse stuff (disease stats, players killed by other players)

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,8 +1,8 @@
 class Stat < ActiveRecord::Base
   self.inheritance_column = 'kind'
-  
+
   validates_presence_of :str, :dex, :con, :int, :mag, :dfn, :dam
-  
+
   @@humanize = {
     :str => "strength",
     :dex => "dexterity",
@@ -11,21 +11,21 @@ class Stat < ActiveRecord::Base
     :mag => "magic",
     :dfn => "defense",
     :dam => "damage"}
-  
+
   def self.human_attr(attr)
     @@humanize[attr]
   end
-  
+
   def save_level_zero
     ( valid_for_level_zero ? save : false )
   end
-  
+
   def add_stats(s)
     Stat.symbols.each{|sym|
       self[sym] += s[sym] }
     self
   end
-  
+
   #true if this object would be a validly distributed free points row
   def valid_distrib(free)
     Stat.symbols.each{|sym|
@@ -40,20 +40,20 @@ class Stat < ActiveRecord::Base
       if points > free
     errors.size == 0
   end
-  
+
   def self.add_stats(s1, s2)
     ret = self.new
     Stat.symbols.each{|sym|
       ret[sym] = s1[sym] + s2[sym] }
     ret
   end
-  
+
   def subtract_stats(s)
     Stat.symbols.each{|sym|
       self[sym] -= s[sym] }
     self
   end
-  
+
   def valid_for_level_zero
     Stat.symbols.each{|sym|
       if self[sym].nil?
@@ -67,7 +67,7 @@ class Stat < ActiveRecord::Base
       if points < 30 || points > 80
     errors.size == 0
   end
-  
+
   def est_level
     pts = self.sum_points
     if pts < 350
@@ -80,21 +80,21 @@ class Stat < ActiveRecord::Base
       pts / 10
     end
   end
-  
+
   def sum_points
     sum = 0
     Stat.symbols.each{|a|
       sum += self[a].to_i }
     sum
   end
-  
+
   def abs_sum_points
     sum = 0
     Stat.symbols.each{|a|
       sum += self[a].to_i.abs }
     sum
   end
-  
+
   #This assumes the stat is a base at level 0
   def to_level(l)
     def self.get_next(m, l)
@@ -103,7 +103,7 @@ class Stat < ActiveRecord::Base
       elsif l < 100
         m + 2*m + (m*(l-20)*0.05).round
       else
-        m + 6*m + (m*(l-100)*(rand*(0.25)+0.05)).round
+        m + 6*m + (m*(l-100)*0.15).round
       end
     end
     new_atr = self.class.new
@@ -113,7 +113,7 @@ class Stat < ActiveRecord::Base
     }
     new_atr
   end
-  
+
   #How many experience points it would require for level
   def exp_for_level(l)
     def self.point_cost(pt, l)
@@ -132,7 +132,7 @@ class Stat < ActiveRecord::Base
     cost += point_cost(Stat.mod_cost(self.owner[:freepts].to_i), l)
     cost
   end
-  
+
   #The more points in something one has, the more expensive additional points become
   def self.mod_cost(m)
     if m < 40
@@ -145,17 +145,17 @@ class Stat < ActiveRecord::Base
       10*m - 1580  #6m - 380 + (m-300)*4
     end
   end
-  
+
   def self.symbols
     [:con,:dam,:dex,:dfn,:int,:mag,:str]
   end
-  
+
   #goes through the stat symbols and looks for them in the given object
   #returns the hash. Stat symbol is zero if no attribute in object
   def self.to_symbols_hash(b)
     Stat.symbols.inject({}) {|h,sym| h.merge( {sym => (b[sym].nil? ? 0 : b[sym])} ) }
   end
-  
+
   #battle related stuff
   def phys_dam
     return (self.str + self.dam + (rand(self.str) * rand(self.dam)) / (self.str+self.dam)).to_i
@@ -165,7 +165,7 @@ class Stat < ActiveRecord::Base
     cdam = (dam - (enemy_dfn + 4 * rand(enemy_dfn)) / 5).to_i
     return (cdam > 0 ? cdam : 0 )
   end
-  
+
   def self.damage_after_mag_res(mag_dam, int, mag)
     mdam = (mag_dam - rand(int * mag) / 100 - mag - int ).to_i
     return (mdam > 0 ? mdam : 0 )
