2086d063872fc564f6a8e99dab1d02f816fca6e4
callahat <tim.callahan25@yahoo.com>
2015-01-22 21:49:54 -0500

    Adding github ribbon, updating google ad code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def google_ad
-'<script type="text/javascript"><!--
-google_ad_client = "pub-3928035645761607";
-/* 120x600, created 5/17/10 */
-google_ad_slot = "0512113450";
-google_ad_width = 120;
-google_ad_height = 600;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+'<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 120x600, created 5/17/10 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:600px"
+     data-ad-client="ca-pub-3928035645761607"
+     data-ad-slot="0512113450"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
 </script>'.html_safe
   end
 end
